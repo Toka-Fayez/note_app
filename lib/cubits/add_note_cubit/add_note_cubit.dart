f36a0c1aa23f27f43e8ae7d5bb4e306f48f7d778
Toka Fayez <tokafayez@gmail.com>
2025-01-30 03:11:24 +0200

    add note method

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:noteapp/contstant.dart';
 import 'package:noteapp/cubits/add_note_cubit/add_note_state.dart';
+import 'package:noteapp/models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) {
+    emit(AddNoteLoading());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteSucess());
+      noteBox.add(note);
+    } catch (e) {
+      AddNoteFailer(errorMaaasage: e.toString());
+    }
+  }
 }
--- lib/widgets/custom_show_model_bottom.dart
+++ lib/widgets/custom_show_model_bottom.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:noteapp/widgets/add_note_bottom_sheet.dart';
-import 'package:noteapp/widgets/customtextfield.dart';
+import 'package:noteapp/widgets/add_note_form.dart';
+
 
 class CustomShowModelBottom extends StatelessWidget {
   const CustomShowModelBottom({super.key});
@@ -16,69 +16,6 @@ class CustomShowModelBottom extends StatelessWidget {
             child: AddNoteForm(),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  GlobalKey<FormState> formKey = GlobalKey();
-
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-
-  String? tilte, subTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          CustomTextField(
-            hint: 'Title',
-            onSaved: (value) {
-              tilte = value;
-            },
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            onSaved: (value) {
-              subTitle = value;
-            },
-            hint: 'Contant',
-            maxLines: 8,
-          ),
-          SizedBox(
-            height: 32,
-          ),
-          AddNoteBottomSheet(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {
-                  
-                });
-              }
-            },
-          ),
-          SizedBox(
-            height: 16,
-          ),
-        ],
       ),
     );
   }
